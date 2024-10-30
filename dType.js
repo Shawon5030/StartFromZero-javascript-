a9f22e7838f3ec3fc963ae4c4b378345ef4cb917
unknown <haquemahmudul600@gmail.com>
2024-10-30 21:43:56 +0600

    commit-22

--- dType.js
+++ dType.js
@@ -1,20 +1,25 @@
-// Numbers:
-let length = 16;
-let weight = 7.5;
+// // Numbers:
+// let length = 16;
+// let weight = 7.5;
 
-// Strings:
-let color = "Yellow";
-let lastName = "Johnson";
+// // Strings:
+// let color = "Yellow";
+// let lastName = "Johnson";
 
-// Booleans
-let x = true;
-let y = false;
+// // Booleans
+// let x = true;
+// let y = false;
 
-// Object:
-const person = {firstName:"John", lastName:"Doe"};
+// // Object:
+// const person = {firstName:"John", lastName:"Doe"};
 
-// Array object:
-const cars = ["Saab", "Volvo", "BMW"];
+// // Array object:
+// const cars = ["Saab", "Volvo", "BMW"];
 
-// Date object:
-const date = new Date("2022-03-25");
+// // Date object:
+// const date = new Date("2022-03-25");
+
+// let x = 16 + "Volvo";
+let x = "Volvo" + 16;
+
+console.log(x); // Output: "16Volvo"
