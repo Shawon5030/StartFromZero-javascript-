85e191233a4da2adee57558f4a179fef5f7e9c85
unknown <haquemahmudul600@gmail.com>
2024-10-31 12:21:47 +0600

    commit-5

--- dom.js
+++ dom.js
@@ -1,3 +1,3 @@
 title = document.querySelector('h1')
 title.textContent = 'Hello'
-title.innerHTML = '<strong style='''>Hello</strong'
+title.innerHTML = '<strong style="color":"red"'>Hello</strong'
