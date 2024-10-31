5041e2f0385f0788b706fba3a238d22b1a5de357
unknown <haquemahmudul600@gmail.com>
2024-10-31 19:10:52 +0600

    commit-1

--- dom.js
+++ dom.js
@@ -1,39 +1,21 @@
-title = document.querySelector('h1')
-// title.textContent = 'Hello'
-// title.innerHTML = '<strong style="color:red">Hello</strong'
-// title.innerText = 'this is shawon'
-
-document.write(title.innerHTML)
-document.write("<br>")
-document.write(title.innerText)
-document.write(title.textContent)
-
-// let a = title.getAttribute('class')
-// document.write(a)
-
-title.setAttribute('class', 'heading')
-// let a = title.getAttribute('class')
-// document.write(a)
-
-link = title.classList.add('heading1')
-let a = title.getAttribute('class')
-document.write(a)
-
-title.setAttribute('title','highlight')
-
-let image = document.querySelector('img')
-console.log(image)
 
 
-let btn1 = document.querySelector('.tamim')
-let btn2 = document.querySelector('.liton')
+document.addEventListener('click', function(){
 
-btn1.addEventListener('click', function(){
-  
-    image.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqElcdFxTkKvVx1gGyiPG7TYgOoVUM02jeag&s"
+
+    let r = Math.floor(Math.random() * 255).toString(16)
+    let g = Math.floor(Math.random() * 255).toString(16)
+    let b = Math.floor(Math.random() * 255).toString(16)
+    let value = document.querySelector("#")
+   
+
+    let rbg = `#${r}${g}${b}`
+    console.log(rbg)
+
+    document.body.style.backgroundColor = rbg
     
-})
 
-btn2.addEventListener("click",()=>{
-    image.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAUNUjFKFeInSOGRZMS_0bXgzQuDDaUWTN04--EsEb0nh7F0dEZq1z618&s"
+
+
+
 })
