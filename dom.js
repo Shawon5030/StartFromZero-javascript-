b5a3ace9f76c559c3614c64367444f495364d132
unknown <haquemahmudul600@gmail.com>
2024-10-31 13:25:51 +0600

    commit-15

--- dom.js
+++ dom.js
@@ -25,10 +25,15 @@ let image = document.querySelector('img')
 console.log(image)
 
 
-let btn1 = document.querySelector('button')
+let btn1 = document.querySelector('.tamim')
+let btn2 = document.querySelector('.liton')
 
 btn1.addEventListener('click', function(){
   
     image.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqElcdFxTkKvVx1gGyiPG7TYgOoVUM02jeag&s"
     
+})
+
+btn2.addEventListener("click",()=>{
+    image.src = "https://www.biography.com/scientist/albert-einstein/images/albert-einstein-1889-1955-1100x625.jpg"
 })
