1c13c3ffcad36e4bfdeea455eb123a5582d24929
unknown <haquemahmudul600@gmail.com>
2024-10-31 19:11:52 +0600

    commit-2

--- dom.js
+++ dom.js
@@ -6,10 +6,12 @@ document.addEventListener('click', function(){
     let r = Math.floor(Math.random() * 255).toString(16)
     let g = Math.floor(Math.random() * 255).toString(16)
     let b = Math.floor(Math.random() * 255).toString(16)
-    let value = document.querySelector("#")
+    let value = document.querySelector("#input")
+    
    
 
     let rbg = `#${r}${g}${b}`
+    value.value = rbg
     console.log(rbg)
 
     document.body.style.backgroundColor = rbg
